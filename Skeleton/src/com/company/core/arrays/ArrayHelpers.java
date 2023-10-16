f4fac6a6feea8a6969f9ed059fe72d3a6f94319d
andreev5r <andrev.petar.biz@gmail.com>
2023-10-16 17:20:30 +0300

    Petar A: ArrayHelpers.java - Add, AddAll Methods updated.
    StringHelpers.java - Abbreviate, Capitalize methods updated.

--- Skeleton/src/com/company/core/arrays/ArrayHelpers.java
+++ Skeleton/src/com/company/core/arrays/ArrayHelpers.java
@@ -3,17 +3,49 @@ package com.company.core.arrays;
 @SuppressWarnings({"ManualArrayCopy", "ExplicitArrayFilling"})
 public class ArrayHelpers {
 
+
+    /**
+     * Appends an element to an int[] array.
+     *
+     * @param source The left part of the new array
+     * @param element The right part of the new array
+     * @return An int[] array that consists of the source array elements followed by the newly added int.
+     *
+     * @author Petar Andreev
+     */
     public static int[] add(int[] source, int element) {
-        return new int[1];
+        int[] array = new int[source.length+1];
+        for (int i = 0; i < source.length; i++) {
+            array[i] = source[i];
+        }
+        array[array.length-1] = element;
+        return array;
     }
 
-    public static int[] addFirst(int[] source, int element) {
+    public static int[] addFirst(int[] source, int element)
+    {
         return new int[1];
     }
-
-    public static int[] addAll(int[] source, int... elements) {
-        return new int[1];
+    /**
+     * Appends an array of elements to an int[] array.
+     *
+     * @param source The left part of the new array
+     * @param elements The right part of the new array
+     * @return An int[] array that consists of the source array elements followed by the newly added integers.
+     *
+     * @author Petar Andreev
+     */
+    public static int[] addAll(int[] source, int... elements){
+    int[] array = new int[source.length + elements.length];
+        for (int i = 0; i < array.length; i++) {
+            if(i < source.length){
+        array[i] = source[i];
+            } else {
+                array[i] = elements [i - source.length];
+            }
     }
+        return array;
+}
 
     public static boolean contains(int[] source, int element) {
         return false;
--- Skeleton/src/com/company/core/strings/StringHelpers.java
+++ Skeleton/src/com/company/core/strings/StringHelpers.java
@@ -2,13 +2,48 @@ package com.company.core.strings;
 
 @SuppressWarnings("StringConcatenationInLoop")
 public class StringHelpers {
-
+    /**
+     * Abbreviates a string, if its length extends the given length.
+     *
+     * @param source The string to be abbreviated
+     * @param maxLength The maximum length, after which the string will be abrevviated, using "..."
+     * @return A string that includes the initial string, up to the selected length, followed by "..."
+     *
+     * @author Petar Andreev
+     */
     public static String abbreviate(String source, int maxLength) {
-        return null;
+        String[] stringArray = source.split("");
+        int length = maxLength;
+        String dots = "...";
+        String result = "";
+        if(stringArray.length < maxLength){
+            result = source;
+        } else if (source.isEmpty()){
+            result = "";
+        } else {
+            for (int i = 0; i < length; i++) {
+                    result += stringArray[i];
+            }
+            result += dots;
+        }
+        return result;
     }
-
+    /**
+     * Capitalizes the first letter in a string.
+     *
+     * @param source The string to be abbreviated
+     * @return The capitalized string.
+     *
+     * @author Petar Andreev
+     */
     public static String capitalize(String source) {
-        return null;
+        String[] stringArray = source.split("");
+        stringArray[0] = stringArray[0].toUpperCase();
+        String result = "";
+        for (int i = 0; i < stringArray.length; i++) {
+            result += stringArray[i];
+        }
+        return result;
     }
 
     public static String concat(String string1, String string2) {
